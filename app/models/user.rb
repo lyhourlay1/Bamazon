d0feb5767103297b555a993147d748a3f12b6815
Lyhour Lay <lyhourlay1@gmail.com>
2022-02-18 14:37:23 -0800

    weekly update

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,8 +19,6 @@ class User < ApplicationRecord
     validates :password, length:{minimum: 6}, allow_nil: true
     validates :email, format: { with: URI::MailTo::EMAIL_REGEXP } 
 
-
-
     after_initialize :ensure_session_token
 
     has_one :cart,
